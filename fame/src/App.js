a06f6df637f113d3e2f596c641eaa14e2aac236d
Rishabh Ganesh <meltingbox91225@gmail.com>
2024-04-01 02:00:56 -0400

    Updated styling and layout

--- fame/src/App.js
+++ fame/src/App.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import logo from './fame.png';
+import ris from './ris.jpg';
 
 function App() {
   return (
     <div className="App">
       <header>
         <img src={logo} alt="Logo" />
-        <h1>Fame</h1>
       </header>
       <main>
         <div className="description">
-          <p>Making the global stage your platform - the next generation of social media.</p>
+          <h4>Connecting the social media world with the global stage.</h4>
         </div>
+        <div className="mid">
+          <h3>Fame is the platform set to revolutionize mainstream social media through its <br></br>World Page 🌎 and Profile Video 🎥 features.</h3>
+        </div>
+        <br></br>
+        <p></p>
         <div className="mid">
           <h1>Meet Our Co-Founders</h1>
         </div>
         <div className="container">
-          <div className="row justify-content-center"> {/* Added justify-content-center class */}
+          <div className="row justify-content-center">
             <div className="col-md-6">
-              <div className="image">
+              <div className="image d-flex justify-content-center align-items-center flex-column">
                 <img src={logo} alt="Image 1" />
-                <p>Charles Grill</p>
+                <h3 className="text-center">Charles Grill</h3>
+                <div>
+                  <p className="text-left">Charlie is an entrepreneur with a background in applied physics and mathematics who is working on building Fame full-time. He enjoys making music, swimming, and cryptocurrency trading. His passion for idea generation, his expertise in composing music, and his experience developing pet projects laid the foundation for Fame.</p>
+                </div>
               </div>
             </div>
             <div className="col-md-6">
-              <div className="image">
-                <img src={logo} alt="Image 2" />
-                <p>Rishabh Ganesh</p>
+              <div className="image d-flex justify-content-center align-items-center flex-column">
+                <img src={ris} alt="Image 2" style={{ width: '100%', maxWidth: '280px' }} />
+                <h3 className="text-center">Rishabh Ganesh</h3> 
+                <div>
+                  <p className="text-left">Rishabh is an innovator with a background in computer science and applied mathematics dedicated to building the technical side of Fame. In his free time, he can be found shuffling cards, running, and creating content. As one of the pioneers for Fame, he is passionate about making a modern, cutting-edge platform focused on its users.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <p></p>
+        <div className="mid">
+          <h1>Contact Us</h1>
+          <p><a href="mailto:rishabh.ganesh@columbia.edu">rishabh.ganesh@columbia.edu</a><br></br><a href="mailto:cwg2120@columbia.edu">cwg2120@columbia.edu</a></p>
+          <p></p>
+        </div>
       </main>
+      <footer className="footer mid">
+        <p>Copyright &copy; 2024</p>
+      </footer>
     </div>
   );
 }
