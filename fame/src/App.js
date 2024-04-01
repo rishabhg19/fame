fae2c7cd15321bb8ef9e91b8b1afd54b08e0f423
Rishabh Ganesh <meltingbox91225@gmail.com>
2024-03-31 23:12:51 -0400

    Modified layout

--- fame/src/App.js
+++ fame/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './fame.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="Logo" />
+        <h1>Fame</h1>
       </header>
+      <main>
+        <div className="description">
+          <p>Making the global stage your platform - the next generation of social media.</p>
+        </div>
+        <div className="mid">
+          <h1>Meet Our Co-Founders</h1>
+        </div>
+        <div className="container">
+          <div className="row justify-content-center"> {/* Added justify-content-center class */}
+            <div className="col-md-6">
+              <div className="image">
+                <img src={logo} alt="Image 1" />
+                <p>Charles Grill</p>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="image">
+                <img src={logo} alt="Image 2" />
+                <p>Rishabh Ganesh</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
